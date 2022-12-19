c5758a3a1fed58caafe5611a3ff18874dfdb2b1b
OreskaG <Ctepanrfo@gmail.com>
2022-12-19 13:32:22 +0300

    fix: zip func

--- func/compress.js
+++ func/compress.js
@@ -1,20 +1,16 @@
 import { pipeline } from 'stream/promises';
 import { createBrotliCompress } from 'zlib';
 import { createReadStream, createWriteStream } from 'fs';
-import { resolve } from 'path';
+import { parse, resolve } from 'path';
 import { EOL } from 'os';
 import { currentDir } from "./currentDirectory.js";
 
 export async function compress(pathFile, pathDest) {
     try {
-        if (!pathDest.endsWith('.br')) {
-            pathDest += '.br';
-        }
-      
         await pipeline(
             createReadStream(resolve(pathFile)),
             createBrotliCompress(),
-            createWriteStream(resolve(pathDest))
+            createWriteStream(resolve(pathDest, `${parse(pathFile).base}.br`))
         );
     } catch {
         console.log('Operation failed', EOL);
